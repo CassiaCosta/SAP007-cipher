fdac912e14dbfd7d3a5f337931fdfab90dbd1816
Cássia Costa <cassia.silva93@hotmail.com>
2022-01-19 18:06:47 -0300

    Funções dos botões feitas

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,21 @@
-var messageEncode = document.getElementById('msgEncode').value;
-var offsetEncode = document.getElementById('offsetE').value;
+//var messageEncode = document.getElementById('msgEncode').value;
+//var offsetEncode = document.getElementById('offsetE').value;
 
 const cipher = {
-  encode: function cipherE(messageEncode, offsetEncode) {
-    let encodeResult = "";
+//  encode: function cipherE(messageEncode, offsetEncode) {
+//    let encodeResult = "";
 
-    for (let i = 0; i < messageEncode.length; i++) {
-      const code = messageEncode.charCodeAt(i);
+//    for (let i = 0; i < messageEncode.length; i++) {
+//      const code = messageEncode.charCodeAt(i);
 
-      if ((code >= 65) && (code <= 90)) {
-        encodeResult = ((code - 65 + offsetEncode) % 26) + 65;
-        encodeResult += String.fromCharCode(code);
-        return encodeResult;
-      } 
-      
-    }
-  },
-
-  //deconde: function cipherD(C, D) {
-  //}
+//      if ((code >= 65) && (code <= 90)) {
+//        encodeResult = ((code - 65 + offsetEncode) % 26) + 65;
+//        encodeResult += String.fromCharCode(code);
+//        return encodeResult;
+//      } 
+//      
+//    }
+// },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,68 @@ import cipher from './cipher.js';
 
 const buttonCode = document.getElementById('btnCodificar');
 
+function codificarMensagem() {
+    var display1 = document.getElementById('cifrar').style.display;
+    if (display1 == 'block') {
+        document.getElementById('cifrar').style.display = 'none';
+    } else {
+        document.getElementById('cifrar').style.display = 'block';
+        document.getElementById('welcome').style.display = 'none';
+    };
+    
+};
+
 buttonCode.addEventListener('click', function () {
-    decifrarMensagem();
+    codificarMensagem();
 });
 
-function decifrarMensagem() {
-    var display = document.getElementById('cifrar').style.display;
-    if (display == "none") {
-        document.getElementById('cifrar').style.display = 'block';
+
+const buttonCode2 = document.getElementById('btnDecodificar');
+
+function decodificarMensagem() {
+    var display2 = document.getElementById('decifrar').style.display;
+    if (display2 == 'block') {
+        document.getElementById('decifrar').style.display = 'none';
     } else {
+        document.getElementById('decifrar').style.display = 'block';
+        document.getElementById('welcome').style.display = 'none';
+    };
+};
+
+buttonCode2.addEventListener('click', function () {
+    decodificarMensagem();
+})
+
+const backC = document.getElementById('voltarC');
+
+function backHome1() {
+    var display3 = document.getElementById('welcome').style.display;
+    if (display3 == 'block') {
+        document.getElementById('welcome').style.display = 'none';
+    } else {
+        document.getElementById('welcome').style.display = 'block';
         document.getElementById('cifrar').style.display = 'none';
     };
     
 };
 
-console.log(cipher);
+backC.addEventListener('click', function () {
+    backHome1();
+})
 
+const backD = document.getElementById('voltarD');
+
+function backHome2() {
+    var display4 = document.getElementById('welcome').style.display;
+    if (display4 == 'block') {
+        document.getElementById('welcome').style.display = 'none';
+    } else {
+        document.getElementById('welcome').style.display = 'block';
+        document.getElementById('decifrar').style.display = 'none';
+    };
+    
+};
+
+backD.addEventListener('click', function () {
+    backHome2();
+})
