1190823754b4839e6872f03a21d7ebb1d4cb3d84
Cássia Costa <cassia.silva93@hotmail.com>
2022-01-19 16:35:03 -0300

    Estrutura HTML do projeto definida e a estilização também

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import cipher from './cipher.js';
 
+const buttonCode = document.getElementById('btnCodificar');
+
+buttonCode.addEventListener('click', function () {
+    decifrarMensagem();
+});
+
+function decifrarMensagem() {
+    var display = document.getElementById('cifrar').style.display;
+    if (display == "none") {
+        document.getElementById('cifrar').style.display = 'block';
+    } else {
+        document.getElementById('cifrar').style.display = 'none';
+    };
+    
+};
+
 console.log(cipher);
+
