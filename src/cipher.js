f1f1253d25913518748ca247f4211d8ed64c923c
Cássia Costa <cassia.silva93@hotmail.com>
2022-02-01 10:43:09 -0300

    Commit final

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,46 @@
-//var messageEncode = document.getElementById('msgEncode').value;
-//var offsetEncode = document.getElementById('offsetE').value;
-
 const cipher = {
-//  encode: function cipherE(messageEncode, offsetEncode) {
-//    let encodeResult = "";
+  encode: function(offset, string) {
+    if(string == "" || string == null || offset == "" || offset == null){ 
+      alert("Insira um deslocamento e texto válidos!");  
+      throw new TypeError("Some informations are missing:", "cipher.js", 4);
+    }
+    let encodedMessage = "";
+    for (let i = 0; i < string.length; i++) {
+      let code = string.charCodeAt(i);
+      if (code >= 65 && code <= 90) {
+        encodedMessage += String.fromCharCode((code - 65 + offset) % 26 + 65);
+      } else if ( code >= 97 && code <= 122) {
+        encodedMessage += String.fromCharCode((code - 97 + offset) % 26 + 97);
+      } else if (code >= 123 && code <= 254) {
+        encodedMessage += String.fromCharCode((code - 123 + offset) % 123 + 123);
+      } else if (string.charCodeAt(i) == 32) {
+        encodedMessage += string.charAt(i);
+      }
+    }
+    return encodedMessage;
+  },
 
-//    for (let i = 0; i < messageEncode.length; i++) {
-//      const code = messageEncode.charCodeAt(i);
-
-//      if ((code >= 65) && (code <= 90)) {
-//        encodeResult = ((code - 65 + offsetEncode) % 26) + 65;
-//        encodeResult += String.fromCharCode(code);
-//        return encodeResult;
-//      } 
-//      
-//    }
-// },
+  
+  decode: function(offset, string) {
+    if(string == "" || string == null || offset == "" || offset == null){ 
+      alert("Insira um deslocamento e texto válidos!");  
+      throw new TypeError("Some informations are missing:", "cipher.js", 25);
+    }
+    let decodedMessage = "";
+    for (let i = 0; i < string.length; i++) {
+      const code = string.charCodeAt(i);
+      if(code >= 65 && code <=90) {
+        decodedMessage += String.fromCharCode((code - 90 - offset) % 26 + 90);
+      } else if(code >= 97 && code <= 122) {
+        decodedMessage += String.fromCharCode((code - 97 - offset) % 26 + 97);
+      } else if (code >= 123 && code <= 254) {
+        decodedMessage += String.fromCharCode((code - 123 - offset % 123) % 123 + 123);
+      } else if(string.charCodeAt(i) == 32) {
+        decodedMessage += string.charAt(i);
+      }
+    }
+    return decodedMessage;
+  }
 };
 
 export default cipher;
-
--- src/index.js
+++ src/index.js
@@ -1,69 +1,80 @@
 import cipher from './cipher.js';
 
-const buttonCode = document.getElementById('btnCodificar');
-
-function codificarMensagem() {
-    var display1 = document.getElementById('cifrar').style.display;
+const btnEncode = document.getElementById('buttonEncode');
+function encodeMessage() {
+    var display1 = document.getElementById('encodeLocation').style.display;
     if (display1 == 'block') {
-        document.getElementById('cifrar').style.display = 'none';
+        document.getElementById('encodeLocation').style.display = 'none';
     } else {
-        document.getElementById('cifrar').style.display = 'block';
-        document.getElementById('welcome').style.display = 'none';
-    };
-    
-};
-
-buttonCode.addEventListener('click', function () {
-    codificarMensagem();
+        document.getElementById('encodeLocation').style.display = 'block';
+        document.getElementById('root').style.display = 'none';
+    }
+}
+btnEncode.addEventListener('click', function () {
+    encodeMessage();
 });
 
 
-const buttonCode2 = document.getElementById('btnDecodificar');
-
-function decodificarMensagem() {
-    var display2 = document.getElementById('decifrar').style.display;
+const btnDecode = document.getElementById('buttonDecode');
+function decodeMessage() {
+    var display2 = document.getElementById('decodeLocation').style.display;
     if (display2 == 'block') {
-        document.getElementById('decifrar').style.display = 'none';
+        document.getElementById('decodeLocation').style.display = 'none';
     } else {
-        document.getElementById('decifrar').style.display = 'block';
-        document.getElementById('welcome').style.display = 'none';
-    };
-};
+        document.getElementById('decodeLocation').style.display = 'block';
+        document.getElementById('root').style.display = 'none';
+    }
+}
+btnDecode.addEventListener('click', function () {
+    decodeMessage();
+});
 
-buttonCode2.addEventListener('click', function () {
-    decodificarMensagem();
-})
 
-const backC = document.getElementById('voltarC');
-
+const back1 = document.getElementById('return1');
 function backHome1() {
-    var display3 = document.getElementById('welcome').style.display;
+    var display3 = document.getElementById('root').style.display;
     if (display3 == 'block') {
-        document.getElementById('welcome').style.display = 'none';
+        document.getElementById('root').style.display = 'none';
     } else {
-        document.getElementById('welcome').style.display = 'block';
-        document.getElementById('cifrar').style.display = 'none';
-    };
-    
-};
-
-backC.addEventListener('click', function () {
+        document.getElementById('root').style.display = 'block';
+        document.getElementById('encodeLocation').style.display = 'none';
+    }   
+}
+back1.addEventListener('click', function () {
     backHome1();
-})
+});
 
-const backD = document.getElementById('voltarD');
 
+const back2 = document.getElementById('return2');
 function backHome2() {
-    var display4 = document.getElementById('welcome').style.display;
+    var display4 = document.getElementById('root').style.display;
     if (display4 == 'block') {
-        document.getElementById('welcome').style.display = 'none';
+        document.getElementById('root').style.display = 'none';
     } else {
-        document.getElementById('welcome').style.display = 'block';
-        document.getElementById('decifrar').style.display = 'none';
-    };
-    
-};
-
-backD.addEventListener('click', function () {
+        document.getElementById('root').style.display = 'block';
+        document.getElementById('decodeLocation').style.display = 'none';
+    }
+}
+back2.addEventListener('click', function () {
     backHome2();
-})
+});
+
+
+const btnEncoding = document.getElementById('code1');
+function encodingMessage () {
+    let string = document.getElementById("messageEncode").value;
+    let offset = Number(document.getElementById("displacement1").value);
+    const encoding = cipher.encode(offset, string);
+    document.getElementById("printLocationEncode").innerHTML = encoding;
+}
+btnEncoding.addEventListener('click', encodingMessage);
+
+
+const btnDecoding = document.getElementById('code2');
+function decodingMessage () {
+    let string = document.getElementById("messageDecode").value;
+    let offset = Number(document.getElementById("displacement2").value);
+    const decoding = cipher.decode(offset, string);
+    document.getElementById("printLocationDecode").innerHTML = decoding;
+}
+btnDecoding.addEventListener('click', decodingMessage);
